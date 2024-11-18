95d37d3583d6d502c80e7cd3f20883e3c5e9c34f
Timothy Treaster <tim@bwsweb.net>
2024-11-17 23:14:16 -0800

    Added Whazzup Service

--- Foresite/Program.cs
+++ Foresite/Program.cs
@@ -1,4 +1,5 @@
 using Foresite.Components;
+using Foresite.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<WhazzupService>();
 
 var app = builder.Build();
 
