ec48dc692431ee32584c0050b09db7240c215fb4
Wes Sleeman <weston@wsleeman.com>
2024-11-18 23:39:58 -0800

    Pushed fix to stupid hardcoded path. 🤦

--- Foresite/Services/CifpService.cs
+++ Foresite/Services/CifpService.cs
@@ -10,7 +10,7 @@ public sealed class CifpService
 {
 	public CIFP Cifp { get; private set; } = CIFP.Load();
 	public ImmutableHashSet<ImmutableArray<Coordinate>> Coastlines { get; private set; } = [..
-		Reader.ReadShapefile(@"C:\Users\westo\OneDrive\Flying\IVAO\XA\Web\Foresite\Foresite\coastline\coastline_i.shp")
+		Reader.ReadShapefile(Path.Combine(Path.GetDirectoryName(Environment.ProcessPath) ?? Environment.CurrentDirectory, "coastline", "coastline_i.shp"))
 		.Select(ring => ring.Select(pt => new Coordinate((decimal)pt.y, (decimal)pt.x)).ToImmutableArray())
 	];
 
